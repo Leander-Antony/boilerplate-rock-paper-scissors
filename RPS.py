85b98b726ebe099f4c18795f6d061b3d14e939e2
Zoro can Code <leander.antony2023@gmail.com>
2025-07-05 16:20:03 +0000

    done

--- RPS.py
+++ RPS.py
@@ -1,10 +1,24 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+def player(prev_play, opponent_history=[], play_order=[{}]):
+    # Update history
+    if prev_play != "":
+        opponent_history.append(prev_play)
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    # Default first move
+    if len(opponent_history) < 2:
+        return "R"
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    # Update play_order map for sequence prediction
+    last_two = "".join(opponent_history[-2:])
+    if last_two not in play_order[0]:
+        play_order[0][last_two] = 0
+    play_order[0][last_two] += 1
 
-    return guess
+    # Predict next opponent move using last opponent move
+    last = opponent_history[-1]
+    options = [last + "R", last + "P", last + "S"]
+    predictions = {k: play_order[0].get(k, 0) for k in options}
+    prediction = max(predictions, key=predictions.get)[-1]
+
+    # Counter the predicted move
+    counter = {'R': 'P', 'P': 'S', 'S': 'R'}
+    return counter[prediction]
